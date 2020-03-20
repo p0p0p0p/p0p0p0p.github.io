77966abb174377e20d1d2b17652764213100d871
p0p0p0p <phloonk@yahoo.ca>
2020-03-20 12:59:50 -0400

    jQuery

--- js/vgmc14.js
+++ js/vgmc14.js
@@ -74,7 +74,7 @@ function renderGraph() {
   });
 
   var layout = Viva.Graph.Layout.forceDirected(graph, {
-      springLength : 500,
+      springLength : 250,
       springCoeff : 0.0008,
       dragCoeff : .08,
       gravity : -10,
